5b23cf5a045c2e1aad3b5612e16386467c4ad85e
shervain123 <81868987+shervain123@users.noreply.github.com>
2021-10-12 21:41:11 +0800

    Update time updater.py

--- time updater.py
+++ time updater.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
-from sys import argv
-x = str(argv[1])
+ass = open("time.txt","r")
+arse = ass.read()
+x = arse[:arse.find(" ")]
+ass.close()
+print(x)
 date_1 = datetime.strptime(x, "%m/%d/%Y")
-EndDate = date_1.today()+timedelta(weeks=6)
+EndDate = date_1+timedelta(weeks=6)
 print("\n\n\n\nTime get from git        : {}    \ntime processed by python : {}    \ntime after 6 weeks       : {}\n\n\n\n".format(x,date_1.strftime("%m/%d/%Y"),EndDate.strftime("%m/%d/%Y")))
-a = open("time.txt","a")
+a = open("time.txt","w")
 a.write(EndDate.strftime("%m/%d/%Y")+" 04:00:00")
 a.close()
