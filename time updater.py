c0a595a44a01b9f3536e237260bc519a54a1f322
shervain123 <81868987+shervain123@users.noreply.github.com>
2021-10-12 22:21:28 +0800

    Update time updater.py

--- time updater.py
+++ time updater.py
@@ -1,12 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 ass = open("time.txt","r")
 arse = ass.read()
 x = arse[:arse.find(" ")]
+today = date.today()
 ass.close()
 print(x)
-date_1 = datetime.strptime(x, "%m/%d/%Y")
-EndDate = date_1+timedelta(weeks=6)
-print("\n\n\n\nTime get from git        : {}    \ntime processed by python : {}    \ntime after 6 weeks       : {}\n\n\n\n".format(x,date_1.strftime("%m/%d/%Y"),EndDate.strftime("%m/%d/%Y")))
-a = open("time.txt","w")
-a.write(EndDate.strftime("%m/%d/%Y")+" 04:00:00")
-a.close()
+print("\n\n\n\nTime get from git        : {}\nCurrent Time             : {}".format(x,today.strftime("%m/%d/%Y")))
+if(x == today.strftime("%m/%d/%Y")):
+    date_1 = datetime.strptime(x, "%m/%d/%Y")
+    EndDate = date_1+timedelta(weeks=6)
+    print("time processed by python : {}    \ntime after 6 weeks       : {}\n\n\n\n".format(date_1.strftime("%m/%d/%Y"),EndDate.strftime("%m/%d/%Y")))
+    a = open("time.txt","w")
+    a.write(EndDate.strftime("%m/%d/%Y")+" 04:00:00")
+    a.close()
+else:
+    print("\nNo time change needed\n\n\n\n")
