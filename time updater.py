81437a1baa29530868de93ac9131eef10aaee853
shervain123 <81868987+shervain123@users.noreply.github.com>
2021-10-13 02:08:23 +0800

    Update time updater.py

--- time updater.py
+++ time updater.py
@@ -5,11 +5,11 @@ x = arse[:arse.find(" ")]
 bex = x[0:3]
 mix = str(int(x[3:5]) - 1)
 afx = x[5:]
-x = bex + mix + afx
+f = bex + mix + afx
 today = date.today()
 ass.close()
 print("\n\n\n\nTime get from git        : {}\nCurrent Time             : {}".format(x,today.strftime("%m/%d/%Y")))
-if(x == today.strftime("%m/%d/%Y")):
+if(f == today.strftime("%m/%d/%Y")):
     date_1 = datetime.strptime(x, "%m/%d/%Y")
     EndDate = date_1+timedelta(weeks=6)
     print("time processed by python : {}    \ntime after 6 weeks       : {}\n\n\n\n".format(date_1.strftime("%m/%d/%Y"),EndDate.strftime("%m/%d/%Y")))
