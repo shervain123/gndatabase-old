790ccbb1d56a5df86801214940c65df8de446a4d
Shervain123 <81868987+shervain123@users.noreply.github.com>
2021-10-13 01:04:57 +0800

    Update time updater.py

--- time updater.py
+++ time updater.py
@@ -4,7 +4,6 @@ arse = ass.read()
 x = arse[:arse.find(" ")]
 today = date.today()
 ass.close()
-print(x)
 print("\n\n\n\nTime get from git        : {}\nCurrent Time             : {}".format(x,today.strftime("%m/%d/%Y")))
 if(x == today.strftime("%m/%d/%Y")):
     date_1 = datetime.strptime(x, "%m/%d/%Y")
