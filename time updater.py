722b6b5b4a60c52de376a43af8a431db75048dcf
shervain123 <81868987+shervain123@users.noreply.github.com>
2021-10-13 08:33:32 +0800

    time change to UTC

--- time updater.py
+++ time updater.py
@@ -2,19 +2,15 @@ from datetime import datetime, timedelta, date
 ass = open("time.txt","r")
 arse = ass.read()
 x = arse[:arse.find(" ")]
-bex = x[0:3]
-mix = str(int(x[3:5]) - 1)
-afx = x[5:]
-f = bex + mix + afx
 today = date.today()
 ass.close()
 print("\n\n\n\nTime get from git        : {}\nCurrent Time             : {}".format(x,today.strftime("%m/%d/%Y")))
-if(f == today.strftime("%m/%d/%Y")):
+if(x == today.strftime("%m/%d/%Y")):
     date_1 = datetime.strptime(x, "%m/%d/%Y")
     EndDate = date_1+timedelta(weeks=6)
     print("time processed by python : {}    \ntime after 6 weeks       : {}\n\n\n\n".format(date_1.strftime("%m/%d/%Y"),EndDate.strftime("%m/%d/%Y")))
     a = open("time.txt","w")
-    a.write(EndDate.strftime("%m/%d/%Y")+" 04:00:00")
+    a.write(EndDate.strftime("%m/%d/%Y")+" 20:00:00")
     a.close()
 else:
     print("\nNo time change needed\n\n\n\n")
