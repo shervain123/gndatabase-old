2056c044b391aca0bdddacbc88ea64c433d674b8
shervain123 <81868987+shervain123@users.noreply.github.com>
2022-03-07 21:17:40 +0800

    server maintence

---
+++ ajs/export.js
@@ -0,0 +1,280 @@
+function strtonum(name){
+    switch(name){
+        case "albedo": return "&01";
+        case "aloy": return "&02";
+        case "amber": return "&03";
+        case "ayaka": return "&04";
+        case "barbara": return "&05";
+        case "beidou": return "&06";
+        case "bennett": return "&07";
+        case "chongyun": return "&08";
+        case "diluc": return "&09";
+        case "diona": return "&10";
+        case "eula": return "&11";
+        case "fischl": return "&12";
+        case "ganyu": return "&13";
+        case "hu tao": return "&14";
+        case "jean": return "&15";
+        case "kaeya": return "&16";
+        case "kazuha": return "&17";
+        case "keqing": return "&18";
+        case "klee": return "&19";
+        case "kokomi": return "&20";
+        case "lisa": return "&21";
+        case "mona": return "&22";
+        case "ningguang": return "&23";
+        case "noelle": return "&24";
+        case "qiqi": return "&25";
+        case "raiden": return "&26";
+        case "razor": return "&27";
+        case "rosaria": return "&28";
+        case "sara": return "&29";
+        case "sayu": return "&30";
+        case "sucrose": return "&31";
+        case "tartaglia": return "&32";
+        case "thoma": return "&33";
+        case "traveler": return "&34";
+        case "venti": return "&35";
+        case "xiangling": return "&36";
+        case "xiao": return "&37";
+        case "xingqiu": return "&38";
+        case "xinyan": return "&39";
+        case "yanfei": return "&40";
+        case "yoimiya": return "&41";
+        case "zhongli": return "&42";
+        case "itto": return "&43";
+        case "gorou": return "&44";
+        case "shenhe": return "&45"
+        case "yunjin": return "&46"
+    }
+}
+function str2hex(name){
+    switch(name){
+        case "AlleyHunter": return "&01";
+        case "AmenomaKageuchi": return "&02";
+        case "AmosBow": return "&03";
+        case "ApprenticeNotes": return "&04";
+        case "AquilaFavonia": return "&05";
+        case "BeginnerProtector": return "&06";
+        case "BlackcliffAgate": return "&07";
+        case "BlackcliffLongsword": return "&08";
+        case "BlackcliffPole": return "&09";
+        case "BlackcliffSlasher": return "&0a";
+        case "BlackcliffWarbow": return "&0b";
+        case "BlackTassel": return "&0c";
+        case "BloodtaintedGreatsword": return "&0d";
+        case "CompoundBow": return "&0e";
+        case "CoolSteel": return "&0f";
+        case "CrescentPike": return "&10";
+        case "DarkIronSword": return "&11";
+        case "Deathmatch": return "&12";
+        case "DebateClub": return "&13";
+        case "DodocoTales": return "&14";
+        case "DragonBane": return "&15";
+        case "DragonspineSpear": return "&16";
+        case "DullBlade": return "&17";
+        case "EbonyBow": return "&18";
+        case "ElegyfortheEnd": return "&19";
+        case "EmeraldOrb": return "&1a";
+        case "EngulfingLightning": return "&1b";
+        case "EverlastingMoonglow": return "&1c";
+        case "EyeofPerception": return "&1d";
+        case "FavoniusCodex": return "&1e";
+        case "FavoniusGreatsword": return "&1f";
+        case "FavoniusLance": return "&20";
+        case "FavoniusSword": return "&21";
+        case "FavoniusWarbow": return "&22";
+        case "FerrousShadow": return "&23";
+        case "FesteringDesire": return "&24";
+        case "FilletBlade": return "&25";
+        case "FreedomSworn": return "&26";
+        case "Frostbearer": return "&27";
+        case "HakushinRing": return "&28";
+        case "Halberd": return "&29";
+        case "Hamayumi": return "&2a";
+        case "HarbingerofDawn": return "&2b";
+        case "HunterBow": return "&2c";
+        case "IronPoint": return "&2d";
+        case "IronSting": return "&2e";
+        case "KatsuragikiriNagamasa": return "&2f";
+        case "KitainCrossSpear": return "&30";
+        case "LionRoar": return "&31";
+        case "LithicBlade": return "&32";
+        case "LithicSpear": return "&33";
+        case "LostPrayertotheSacredWinds": return "&34";
+        case "LuxuriousSeaLord": return "&35";
+        case "MagicGuide": return "&36";
+        case "MappaMare": return "&37";
+        case "MemoryofDust": return "&38";
+        case "Messenger": return "&39";
+        case "MistsplitterReforged": return "&3a";
+        case "MitternachtsWaltz": return "&3b";
+        case "OldMercPal": return "&3c";
+        case "OtherworldlyStory": return "&3d";
+        case "PocketGrimoire": return "&3e";
+        case "Predator": return "&3f";
+        case "PrimordialJadeCutter": return "&40";
+        case "PrimordialJadeWingedSpear": return "&41";
+        case "PrototypeAmber": return "&42";
+        case "PrototypeArchaic": return "&43";
+        case "PrototypeCrescent": return "&44";
+        case "PrototypeRancour": return "&45";
+        case "PrototypeStarglitter": return "&46";
+        case "Rainslasher": return "&47";
+        case "RavenBow": return "&48";
+        case "RecurveBow": return "&49";
+        case "RoyalBow": return "&4a";
+        case "RoyalGreatsword": return "&4b";
+        case "RoyalGrimoire": return "&4c";
+        case "RoyalLongsword": return "&4d";
+        case "RoyalSpear": return "&4e";
+        case "Rust": return "&4f";
+        case "SacrificialBow": return "&50";
+        case "SacrificialFragments": return "&51";
+        case "SacrificialGreatsword": return "&52";
+        case "SacrificialSword": return "&53";
+        case "SeasonedHunterBow": return "&54";
+        case "SerpentSpine": return "&55";
+        case "SharpshooterOath": return "&56";
+        case "SilverSword": return "&57";
+        case "SkyriderGreatsword": return "&58";
+        case "SkyriderSword": return "&59";
+        case "SkywardAtlas": return "&5a";
+        case "SkywardBlade": return "&5b";
+        case "SkywardHarp": return "&5c";
+        case "SkywardPride": return "&5d";
+        case "SkywardSpine": return "&5e";
+        case "Slingshot": return "&5f";
+        case "SnowTombedStarsilver": return "&60";
+        case "SolarPearl": return "&61";
+        case "SongofBrokenPines": return "&62";
+        case "StaffofHoma": return "&63";
+        case "SummitShaper": return "&64";
+        case "SwordofDescension": return "&65";
+        case "TheAlleyFlash": return "&66";
+        case "TheBell": return "&67";
+        case "TheBlackSword": return "&68";
+        case "TheCatch": return "&69";
+        case "TheFlute": return "&6a";
+        case "TheStringless": return "&6b";
+        case "TheUnforged": return "&6c";
+        case "TheViridescentHunt": return "&6d";
+        case "TheWidsith": return "&6e";
+        case "ThrillingTalesofDragonSlayers": return "&6f";
+        case "ThunderingPulse": return "&70";
+        case "TravelerHandySword": return "&71";
+        case "TwinNephrite": return "&72";
+        case "VortexVanquisher": return "&73";
+        case "WasterGreatsword": return "&74";
+        case "Whiteblind": return "&75";
+        case "WhiteIronGreatsword": return "&76";
+        case "WhiteTassel": return "&77";
+        case "WindblumeOde": return "&78";
+        case "WineandSong": return "&79";
+        case "WolfGravestone": return "&7a";
+        case "MouunMoon": return "&7b"
+        case "PolarStar": return "&7c"
+        case "WavebreakerFin": return "&7d"
+        case "Akuoumaru": return "&7e"
+        case "RedhornStonethresher": return "&7f"
+        case "CinnabarSpindle": return "&80"
+        case "CalamityQueller": return "&81"
+    
+    }
+}
+function copydee(){
+    var text = document.getElementById("exporto")
+    copyToClipboard(text.value);
+    var ico = document.getElementById("copi")
+    copi.innerText="check"
+    setTimeout(function(){copi.innerText="content_copy"},1000)
+  }
+  function copyToClipboard(textToCopy) {
+  // navigator clipboard api needs a secure context (https)
+  if (navigator.clipboard && window.isSecureContext) {
+      // navigator clipboard api method'
+      return navigator.clipboard.writeText(textToCopy);
+  } else {
+      // text area method
+      let textArea = document.createElement("textarea");
+      textArea.value = textToCopy;
+      // make the textarea out of viewport
+      textArea.style.position = "fixed";
+      textArea.style.left = "-999999px";
+      textArea.style.top = "-999999px";
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      return new Promise((res, rej) => {
+          // here the magic happens
+          document.execCommand('copy') ? res() : rej();
+          textArea.remove();
+      });
+  }
+}
+var concharacter = ""
+var conweapon = ""
+function generate(){
+  concharacter = ""
+  conweapon = ""
+  var cookies = listCookies()
+  var i = 0
+  var chadiv = document.getElementById("exportitems")
+  chadiv.innerHTML = ""
+  for (i = 0; i < cookies.length; i++) {
+    var name = getCookie(cookies[i])
+    if(name == '1'){
+      if(concharacter == ""){
+        concharacter = strtonum(cookies[i])
+      }else{
+        concharacter = concharacter + strtonum(cookies[i])
+      }
+      characterimagev(cookies[i],"exportitems")
+    }
+    if(name == '2'){
+        if(conweapon == ""){
+            conweapon = str2hex(cookies[i])
+        }else{
+            conweapon = conweapon + str2hex(cookies[i])
+        }
+        weaponimagev(cookies[i],"exportitems")
+    }
+  }
+  var temname = document.getElementById("teamname").value
+  if(temname == "") temname = "Team1"
+  var check = document.getElementById("exportype")
+  var exporti
+  if(check.checked == true) exporti = "import=0;viewonly=1"
+  else exporti = "import=1;viewonly=0"
+  var texout = document.getElementById("exporto")
+  var exporttext = "https://gndatabase.ml/import.html?" + exporti + ";name=" + temname + ";cha" + concharacter + "wea" + conweapon
+  texout.value=encodeURI(exporttext) 
+}
+
+function generate_link(){
+  concharacter = ""
+  conweapon = ""
+  var cookies = listCookies()
+  var i = 0
+  for (i = 0; i < cookies.length; i++) {
+    var name = getCookie(cookies[i])
+    if(name == '1'){
+      if(concharacter == ""){
+        concharacter = strtonum(cookies[i])
+      }else{
+        concharacter = concharacter + strtonum(cookies[i])
+      }
+    }
+    if(name == '2'){
+        if(conweapon == ""){
+            conweapon = str2hex(cookies[i])
+        }else{
+            conweapon = conweapon + str2hex(cookies[i])
+        }
+    }
+  }
+  var temname = "Team1"
+  var exporti = "import=0;viewonly=1"
+  var exporttext = "https://gndatabase.ml/import.html?" + exporti + ";name=" + temname + ";cha" + concharacter + "wea" + conweapon
+  return encodeURI(exporttext) 
+}
--- js/gnimport.js
+++
@@ -1,9 +0,0 @@
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-function getCharacterCookie(){
-    return document.cookie
-}
